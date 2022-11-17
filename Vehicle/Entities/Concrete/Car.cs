64e546ae468b746be92260a33e523039a43a02cd
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2022-11-17 21:46:56 +0300

    entity

---
+++ Vehicle/Entities/Concrete/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car  : Vehicle
+    {
+        public  int Whells { get; set; }
+        public bool Headlights { get; set; }
+    }
+}
