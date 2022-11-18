64d43fe82e707cfbbfb862a5cfac8e12d5f40723
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2022-11-18 14:15:04 +0300

    repository

---
+++ Vehicle/DataAccess/Concrete/EntityFramework/Abstract/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Abstract
+{
+    public interface IEntityRepository<T> where T : class , new()
+    {
+        List<T> GetAll(Expression<Func<T , bool>> filtet = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void UpDate(T entity);
+        void Delete(T entity);  
+
+    }
+}
